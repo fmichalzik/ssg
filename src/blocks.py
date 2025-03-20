4cfc7aee061ab4bb005b50f0287bac65d32e8c27
fmichalzik <51121813+fmichalzik@users.noreply.github.com>
2025-03-20 11:10:58 +0100

    adds function for block identification

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,69 @@
+from enum import Enum
+
+# Our static site generator supports following types of markdown blocks
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+# function that takes a single block of markdown text as input
+# and returns the BlockType representing the type of block it is
+def block_to_block_type(block):
+    # Headings start with 1-6 # characters, followed by a space
+
+    # if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        # return BlockType.HEADING
+        
+    if block[:2] == "# ":
+        return BlockType.HEADING
+    if block[:3] == "## ":
+        return BlockType.HEADING
+    if block[:4] == "### ":
+        return BlockType.HEADING
+    if block[:5] == "#### ":
+        return BlockType.HEADING
+    if block[:6] == "##### ":
+        return BlockType.HEADING
+    if block[:7] == "###### ":
+        return BlockType.HEADING
+    
+    # Code blocks must start with 3 backticks and end with 3 backticks
+    if block[:3] == "```" and block[-3:] == "```":
+        return BlockType.CODE
+
+    # if none of the above returns, we check for every line in a block   
+    lines = block.split("\n")
+    true_quote = 0
+    true_unordered_list = 0
+    true_ordered_list = 0
+    for line in lines:
+        # Every line in a quote block must start with a > character.
+        if line[:1] == ">":
+            true_quote += 1
+        # Every line in an unordered list block must start with a - character
+        if line[:2] == "- ":
+            true_unordered_list += 1  
+        # Every line in an ordered list block must start with a number 
+        # followed by a . character and a space
+        # The number must start at 1 
+        if (line[:3] == f"{lines.index(line) + 1}. " 
+            # and increment by 1 for each line
+            and line[:1] == f"{true_ordered_list + 1}"):
+            true_ordered_list += 1
+    if len(lines) == true_quote:
+        return BlockType.QUOTE
+    if len(lines) == true_unordered_list:
+        return BlockType.UNORDERED_LIST   
+    if len(lines) == true_ordered_list:
+        return BlockType.ORDERED_LIST
+    
+    # if none of the above conditions are met, the block is a normal paragraph
+    return BlockType.PARAGRAPH
+    
+
 # It takes a raw Markdown string (representing a full document) as input and 
 # returns a list of "block" strings.
 def markdown_to_blocks(markdown) :
@@ -13,5 +79,4 @@ def markdown_to_blocks(markdown) :
     for block in blocks[:]:
         if not block:
             blocks.remove(block)   
-            function
     return blocks
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, BlockType, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -33,5 +33,103 @@ This is the same paragraph on a new line
         blocks = markdown_to_blocks(md)
         self.assertEqual(blocks, [],)
         
+    def test_block_eq_heading_1(self):
+        block = "# This is a header"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_eq_heading_2(self):
+        block = "##### This is a header"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_not_heading_1(self):
+        block = "#This is not a header so will be paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_eq_code_1(self):
+        block = "```This is valid code block´```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+    
+    def test_block_eq_code_2(self):
+        block = "```            This is valid code block´```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+    
+    def test_block_eq_code_3(self):
+        block = "```" \
+        "This is valid code block´" \
+        "```" \
+        ""
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    def test_block_not_code_1(self):
+        block = "``This is not valid code block wo will be paragraph```"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    
+    def test_block_not_code_2(self):
+        block = " ```This is not valid code block wo will be paragraph```"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_eq_quote(self):
+        block = """>1quote
+># quote
+> quote
+> quote
+>quote"""
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+    def test_block_not_quote(self):
+        block = """>1quote
+# quote
+> quote
+> quote
+>not valid so wille be paragraph"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_eq_unordered_list(self):
+        block = """- quote
+- ### quote
+- quote
+- quote
+-  valid unordered list"""
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    def test_block_not_unordered_list(self):
+        block = """- quote
+- ### quote
+- quote
+- quote
+-invalid unordered list so will be paragraph"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_eq_ordered_list(self):
+        block = """1. quote
+2. ### quote
+3. quote
+4. quote
+5.  valid ordered list"""
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+    def test_block_not_ordered_list_1(self):
+        block = """1. quote
+2. ### quote
+3. quote
+4. quote
+5.invalid ordered list so will be paragraph"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    
+    def test_block_not_ordered_list_2(self):
+        block = """1. quote
+2. ### quote
+3. quote
+4. quote
+5 invalid ordered list so will be paragraph"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    
+    def test_block_not_ordered_list_3(self):
+        block = """1. quote
+2. ### quote
+3. quote
+4. quote
+4. invalid ordered list so will be paragraph"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    
 if __name__ == "__main__":
     unittest.main()
